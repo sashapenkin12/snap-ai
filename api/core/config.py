771bd69f28239e51f908b6bf82987eee845e64bd
aleksanr_penkin <sashapenkin12@mail.ru>
2025-09-16 16:57:14 +0600

    feat(environment): implement AppSettings class for working with environment settings in config.py

---
+++ api/core/config.py
@@ -0,0 +1,31 @@
+"""
+App config module.
+"""
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class AppSettings(BaseSettings):
+    """
+    Settings class for environment variables.
+
+    Attributes:
+
+    """
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return 'postgres+asyncpg://{username}:{password}@{host}/{name}'.format(
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            name=self.DB_NAME,
+        )
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = AppSettings() #type: ignore
