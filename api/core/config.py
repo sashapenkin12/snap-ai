adc4f24962dd8b40aaa443d8ff560707d31b5a36
aleksanr_penkin <sashapenkin12@mail.ru>
2025-09-17 15:08:21 +0600

    feat(environment): add access and refresh tokens expiring time settings & add passlib dependency

--- api/core/config.py
+++ api/core/config.py
@@ -9,12 +9,21 @@ class AppSettings(BaseSettings):
     Settings class for environment variables.
 
     Attributes:
+        DB_USER: Database user's name.
+        DB_PASSWORD: Database user's password.
+        DB_HOST: Address of the database.
+        DB_NAME: Name of the database.
 
+        ACCESS_TOKEN_EXPIRE_MINUTES: How long JWT access tokens will be valid.
+        REFRESH_TOKEN_EXPIRE_DAYS: How long JWT refresh tokens will be valid.
     """
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_NAME: str
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 14
 
     @property
     def DATABASE_URL(self) -> str:
