4293d089a23202f897ba8f4c60615ba9eb39626c
aleksanr_penkin <sashapenkin12@mail.ru>
2025-09-18 20:57:28 +0600

    feat(users): implement Profile model

--- api/models/user.py
+++ api/models/user.py
@@ -1,7 +1,24 @@
+from datetime import date
+from enum import StrEnum
+
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, Date, Enum
+
 
 from api.core.database import Base
+
+
+class ActivityEnum(StrEnum):
+    sedentary = 'sedentary'
+    normal = 'normal'
+    sportive = 'sportive'
+
+
+class GoalEnum(StrEnum):
+    lose_weight = 'lose_weight'
+    support_weight = 'support_weight'
+    gain_weight = 'gain_weight'
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -14,3 +31,21 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
 
     is_active: Mapped[bool] = mapped_column(default=False)
+
+
+class Profile(Base):
+    __tablename__ = 'profiles'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    weight: Mapped[int] = mapped_column(nullable=False)
+    height: Mapped[int] = mapped_column(nullable=False)
+
+    birth_date: Mapped[date] = mapped_column(Date, nullable=False)
+
+    activity_type: Mapped[ActivityEnum] = mapped_column(Enum, default=ActivityEnum.normal, nullable=False)
+
+    allergy: Mapped[str] = mapped_column(nullable=False)
+
+    goal: Mapped[GoalEnum] = mapped_column(Enum, default=GoalEnum.support_weight, nullable=False)
+    desired_weight: Mapped[int] = mapped_column()
