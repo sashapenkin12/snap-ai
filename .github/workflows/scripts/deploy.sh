afd7852aa7b1fef4cefd8cb54f1d3ee0380f2f85
aleksanr_penkin <sashapenkin12@mail.ru>
2025-09-19 18:05:47 +0600

    feat(CD): implement CD workflow with Github Secrets variables

---
+++ .github/workflows/scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+cd ~/app
+
+echo "Create .env from Github Secrets"
+cat > .env < EOL
+DB_USER=${{ secrets.DB_USER }}
+DB_PASSWORD=${{ secrets.DB_PASSWORD }}
+DB_HOST=${{ secrets.DB_HOST }}
+DB_NAME=${{ secrets.DB_NAME }}
+SECRET_KEY=${{ secrets.SECRET_KEY}}
+ALGORITHM=${{ secrets.ALGORITHM }}
+EOL
+
+docker-compose down
+docker compose up -d --build
