c837d27fc1612c07525f8be7a5013720c64f1ace
aleksanr_penkin <sashapenkin12@mail.ru>
2025-09-19 18:23:27 +0600

    fix(CD): fix deploy script

--- .github/workflows/scripts/deploy.sh
+++ .github/workflows/scripts/deploy.sh
@@ -5,12 +5,12 @@ cd ~/app
 
 echo "Create .env from Github Secrets"
 cat > .env << EOL
-DB_USER=${{ secrets.DB_USER }}
-DB_PASSWORD=${{ secrets.DB_PASSWORD }}
-DB_HOST=${{ secrets.DB_HOST }}
-DB_NAME=${{ secrets.DB_NAME }}
-SECRET_KEY=${{ secrets.SECRET_KEY}}
-ALGORITHM=${{ secrets.ALGORITHM }}
+DB_USER=${DB_USER}
+DB_PASSWORD=${DB_PASSWORD}
+DB_HOST=${DB_HOST}
+DB_NAME=${DB_NAME}
+SECRET_KEY=${SECRET_KEY}
+ALGORITHM=${ALGORITHM}
 EOL
 
 docker-compose down
